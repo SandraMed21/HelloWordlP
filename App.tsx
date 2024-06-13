e467fbdf0aaac24251078f5bf4377091ac6ae8b0
SandraMed21 <121471558+SandraMed21@users.noreply.github.com>
2024-06-13 01:02:33 -0600

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <p>Sandra Medina</p>
+      <p>19460699</p>
+      <p>React + Vercel</p>
+    </>
+  )
+}
+
+export default App
